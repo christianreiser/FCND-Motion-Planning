80f75703c90a1f024fac9d367470245f2ab9ce97
Christian Reiser <reisercg@gmial.com>
2018-06-10 19:17:04 +0200

    edited A*, added read_home, and remove unnecessary WP

--- planning_utils.py
+++ planning_utils.py
@@ -1,7 +1,9 @@
+import numpy as np
+import re
+
 from enum import Enum
 from queue import PriorityQueue
-import numpy as np
-
+from math import sqrt
 
 def create_grid(data, drone_altitude, safety_distance):
     """
@@ -40,7 +42,6 @@ def create_grid(data, drone_altitude, safety_distance):
 
     return grid, int(north_min), int(east_min)
 
-
 # Assume all actions cost the same.
 class Action(Enum):
     """
@@ -50,20 +51,21 @@ class Action(Enum):
     to the current grid position. The third and final value
     is the cost of performing the action.
     """
-
     WEST = (0, -1, 1)
     EAST = (0, 1, 1)
     NORTH = (-1, 0, 1)
+    NORTH_EAST = (-1, 1, sqrt(2))
+    NORTH_WEST = (-1, -1, sqrt(2))
     SOUTH = (1, 0, 1)
+    SOUTH_EAST = (1, 1, sqrt(2))
+    SOUTH_WEST = (1, -1, sqrt(2))
 
     @property
     def cost(self):
         return self.value[2]
-
     @property
     def delta(self):
         return (self.value[0], self.value[1])
-
 
 def valid_actions(grid, current_node):
     """
@@ -75,7 +77,6 @@ def valid_actions(grid, current_node):
 
     # check if the node is off the grid or
     # it's an obstacle
-
     if x - 1 < 0 or grid[x - 1, y] == 1:
         valid_actions.remove(Action.NORTH)
     if x + 1 > n or grid[x + 1, y] == 1:
@@ -84,12 +85,21 @@ def valid_actions(grid, current_node):
         valid_actions.remove(Action.WEST)
     if y + 1 > m or grid[x, y + 1] == 1:
         valid_actions.remove(Action.EAST)
-
+    if x + 1 > n or y + 1 > m or grid[x + 1, y + 1] == 1:
+        valid_actions.remove(Action.SOUTH_EAST)
+    if x - 1 < 0 or y + 1 > m or grid[x - 1, y + 1] == 1:
+        valid_actions.remove(Action.NORTH_EAST)
+    if x + 1 > n or y - 1 < 0 or grid[x + 1, y - 1] == 1:
+        valid_actions.remove(Action.SOUTH_WEST)
+    if x - 1 < 0 or y - 1 < 0 or grid[x - 1, y - 1] == 1:
+        valid_actions.remove(Action.NORTH_WEST)
     return valid_actions
 
-
 def a_star(grid, h, start, goal):
-
+    """
+    Given a grid and heuristic function returns
+    the lowest cost path from start to goal.
+    """
     path = []
     path_cost = 0
     queue = PriorityQueue()
@@ -98,32 +108,28 @@ def a_star(grid, h, start, goal):
 
     branch = {}
     found = False
-    
+
     while not queue.empty():
         item = queue.get()
+        current_cost = item[0]
         current_node = item[1]
-        if current_node == start:
-            current_cost = 0.0
-        else:              
-            current_cost = branch[current_node][0]
-            
-        if current_node == goal:        
+
+        if current_node == goal:
             print('Found a path.')
             found = True
             break
         else:
-            for action in valid_actions(grid, current_node):
-                # get the tuple representation
-                da = action.delta
-                next_node = (current_node[0] + da[0], current_node[1] + da[1])
-                branch_cost = current_cost + action.cost
-                queue_cost = branch_cost + h(next_node, goal)
-                
-                if next_node not in visited:                
-                    visited.add(next_node)               
-                    branch[next_node] = (branch_cost, current_node, action)
-                    queue.put((queue_cost, next_node))
-             
+            # Get the new vertexes connected to the current vertex
+            for a in valid_actions(grid, current_node):
+                next_node = (current_node[0] + a.delta[0], current_node[1] + a.delta[1])
+                new_cost = current_cost + a.cost + h(next_node, goal)
+
+                if next_node not in visited:
+                    visited.add(next_node)
+                    queue.put((new_cost, next_node))
+
+                    branch[next_node] = (new_cost, current_node, a)
+
     if found:
         # retrace steps
         n = goal
@@ -136,11 +142,52 @@ def a_star(grid, h, start, goal):
     else:
         print('**********************')
         print('Failed to find a path!')
-        print('**********************') 
+        print('**********************')
     return path[::-1], path_cost
-
-
 
 def heuristic(position, goal_position):
     return np.linalg.norm(np.array(position) - np.array(goal_position))
 
+def collinearity_prune(path, epsilon=1e-5):
+    """
+    Prune path points from `path` using collinearity.
+    """
+    def collinearity_check(p1, p2, p3):
+        m = np.concatenate((p1, p2, p3), 0)
+        det = np.linalg.det(m)
+        return abs(det) < epsilon
+
+    def point(p):
+        return np.array([p[0], p[1], 1.]).reshape(1, -1)
+
+    pruned_path = [p for p in path]
+    i = 0
+    while i < len(pruned_path) - 2:
+        p1 = point(pruned_path[i])
+        p2 = point(pruned_path[i+1])
+        p3 = point(pruned_path[i+2])
+
+        # If the 3 points are in a line remove
+        # the 2nd point.
+        # The 3rd point now becomes and 2nd point
+        # and the check is redone with a new third point
+        # on the next iteration.
+        if collinearity_check(p1, p2, p3):
+            # Something subtle here but we can mutate
+            # `pruned_path` freely because the length
+            # of the list is check on every iteration.
+            pruned_path.remove(pruned_path[i+1])
+        else:
+            i += 1
+    return pruned_path
+def read_home(filename):
+    """
+    Reads home (lat, lon) from the first line of the `file`.
+    """
+    with open(filename) as f:
+        first_line = f.readline()
+    match = re.match(r'^lat0 (.*), lon0 (.*)$', first_line)
+    if match:
+        lat = match.group(1)
+        lon = match.group(2)
+    return np.fromstring(f'{lat},{lon}', dtype='Float64', sep=',')
